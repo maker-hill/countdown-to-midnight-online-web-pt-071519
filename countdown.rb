34be58314bb2fa6cb581626d14e950020663ea7c
Jeremy Hill <j.c.hillmail@gmail.com>
2019-07-20 20:51:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,15 @@
-def countdown_with_sleep(second)#write your code here
-    while second > 0 
-      sleep (1.second)
-      second -= 1
-  end
-end  
-def countdown(number)
+ def countdown(number)
   while number > 0 do
     puts "#{number} SECOND(S)!"
       number -= 1 
       
   end
-  return "HAPPY NEW YEAR!"
 end
+ def countdown_with_sleep(second)#write your code here
+    while second > 0 
+      sleep (1.second)
+      second -= 1
+  end
+end 
+
+return 
